7055bada2312262e2555ac2a35b94f2c20a8811e
GitGudwl <95174089+GitGudwl@users.noreply.github.com>
2022-02-20 20:43:53 +0700

    Update soal3_w3.java

--- MG3/soal3_w3.java
+++ MG3/soal3_w3.java
@@ -14,9 +14,10 @@ public class soal3_w3 {
         char operator; // opetator operasi
         
         // algoritma //
-        angka1 = keyboard.nextInt();
-        operator = keyboard.next().charAt(0); // input operator
-        angka2 = keyboard.nextInt();
+        angka1 = sc.nextInt();
+        operator = sc.next().charAt(0); // input operator
+        angka2 = sc.nextInt();
+        sc.close();
         if ((angka1>=1 && angka1<=1000) && (angka2>=1 && angka2<=1000)){
             switch (operator){
                 case '+' : System.out.println(angka1+angka2);break;
